3c65c1fb0c47e6e5ecaf23920dec4e6c8a3bf07e
MaurizioCr <crispinomaurizio034@gmail.com>
2024-06-18 19:42:26 +0200

    Nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import MyNavbar from "./Component/MyNavbar"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
--- src/Component/MyNavbar.jsx
+++ src/Component/MyNavbar.jsx
@@ -1,9 +1,20 @@
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+
 const MyNavbar = () => {
 
     return (
         <>
+            <Navbar bg="primary" data-bs-theme="dark" className="fs-5">
         
-            <a style={{textDecoration: 'none'}} href="https://www.regione.campania.it/">Regione Campania</a>
+        
+          <Navbar.Brand href="#home" className="me-5"> <img src="https://www.aslnapoli2nord.it/templates/shaper_educon/images/presets/preset1/logo.png"/> </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="https://www.regione.campania.it/"> <img style={{verticalAlign: "text-top"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Regione-Campania-Stemma.svg/1200px-Regione-Campania-Stemma.svg.png" width={20 + "em"}/> Regione Campania</Nav.Link>
+            <Nav.Link href="#features">Amministrazione Trasparente</Nav.Link>
+            <Nav.Link href="#pricing">Albo Pretorio</Nav.Link>
+          </Nav>
+        
+      </Navbar>
         </>
     );
 };
