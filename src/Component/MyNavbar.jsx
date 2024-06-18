eb988df21c56791218ad1a5c065dbe79ba8fa83c
MaurizioCr <crispinomaurizio034@gmail.com>
2024-06-18 19:18:20 +0200

    Inizio progetto

---
+++ src/Component/MyNavbar.jsx
@@ -0,0 +1,11 @@
+const MyNavbar = () => {
+
+    return (
+        <>
+        
+            <a style={{textDecoration: 'none'}} href="https://www.regione.campania.it/">Regione Campania</a>
+        </>
+    );
+};
+
+export default MyNavbar;
