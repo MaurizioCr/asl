d303709af4ac7029e4044b30674ddeba3e3df947
MaurizioCr <crispinomaurizio034@gmail.com>
2024-06-26 15:54:20 +0200

    width

--- src/Component/MyNavbar.jsx
+++ src/Component/MyNavbar.jsx
@@ -7,7 +7,7 @@ const MyNavbar = () => {
             <Navbar bg="primary" data-bs-theme="dark" className="fs-5">
         
         
-          <Navbar.Brand href="#home" className="me-5"> <img width={85 + "em"} src="https://www.aslnapoli2nord.it/templates/shaper_educon/images/presets/preset1/logo.png"/> </Navbar.Brand>
+          <Navbar.Brand href="#home" className="me-5"> <img width={65 + "em"} src="https://www.aslnapoli2nord.it/templates/shaper_educon/images/presets/preset1/logo.png"/> </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="https://www.regione.campania.it/"> <img style={{verticalAlign: "text-top"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Regione-Campania-Stemma.svg/1200px-Regione-Campania-Stemma.svg.png" width={20 + "em"}/> Regione Campania</Nav.Link>
             <Nav.Link href="#">Amministrazione Trasparente</Nav.Link>
