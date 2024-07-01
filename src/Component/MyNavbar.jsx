3fca9ceb8a5af081f21a23a600945bc61ba77899
MaurizioCr <crispinomaurizio034@gmail.com>
2024-07-01 16:01:25 +0200

    text-white

--- src/Component/MyNavbar.jsx
+++ src/Component/MyNavbar.jsx
@@ -13,17 +13,17 @@ const MyNavbar = () => {
 
     return (
         <>
-            <Navbar bg="primary" data-bs-theme="dark" className="fs-5">
+            <Navbar bg="primary" data-bs-theme="dark" className="fs-5 text-white">
         
         
           <Navbar.Brand href="#home" className="me-5"> <img width={65 + "em"} src="https://www.aslnapoli2nord.it/templates/shaper_educon/images/presets/preset1/logo.png"/> </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="https://www.regione.campania.it/"> <img style={{verticalAlign: "text-top"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Regione-Campania-Stemma.svg/1200px-Regione-Campania-Stemma.svg.png" width={20 + "em"}/> Regione Campania</Nav.Link>
-            <Nav.Link href="#">Amministrazione Trasparente</Nav.Link>
-            <Nav.Link href="#">Albo Pretorio</Nav.Link>
+            <Nav.Link className="text-white" href="https://www.regione.campania.it/"> <img style={{verticalAlign: "text-top"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Regione-Campania-Stemma.svg/1200px-Regione-Campania-Stemma.svg.png" width={20 + "em"}/> Regione Campania</Nav.Link>
+            <Nav.Link className="text-white" href="#">Amministrazione Trasparente</Nav.Link>
+            <Nav.Link className="text-white" href="#">Albo Pretorio</Nav.Link>
           </Nav>
           <Nav className="text-right me-3">
-            <Nav.Link href="#"><img  width={30 + "em"} src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"/>Area Dipendenti</Nav.Link> 
+            <Nav.Link className="text-white" href="#"><img  width={30 + "em"} src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"/>Area Dipendenti</Nav.Link> 
           </Nav>
           <Form className="d-flex">
                   <Form.Control
@@ -48,7 +48,7 @@ const MyNavbar = () => {
         
       </Navbar>
 
-      <Container className="rounded text-center bg-primary mt-3 py-3 w-75">
+      <Container className="rounded text-center text-white bg-primary mt-3 py-3 w-75">
             <Row>
                 <Col>
                     <Nav.Link>Asl a casa tua</Nav.Link>
