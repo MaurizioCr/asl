b1e9cfe9e710f5d295ec91d93963eaaec53c869b
MaurizioCr <crispinomaurizio034@gmail.com>
2024-07-09 14:43:34 +0200

    Up a schermo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import MyNavbar from "./Component/MyNavbar"
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Up from './Component/up';
 
 function App() {
   return (
     <div>
       <MyNavbar/>
+      <Up/>
     </div>
   );
 }
