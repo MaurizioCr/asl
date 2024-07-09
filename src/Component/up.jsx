dae4150272f8f9c042e064e8a614dfd8e77368aa
MaurizioCr <crispinomaurizio034@gmail.com>
2024-07-09 14:43:20 +0200

    Button up

---
+++ src/Component/up.jsx
@@ -0,0 +1,14 @@
+import { Button } from "react-bootstrap";
+
+const up = () => {
+    return (
+        <>
+            <Button className="position-absolute bottom-0 end-0 bg-primary">
+                
+            </Button>
+        </>
+    );
+};
+    
+export default up;
+    
