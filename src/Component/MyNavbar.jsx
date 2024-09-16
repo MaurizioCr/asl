80dc012ab120f28a098dd1e6269fbee447eb27d6
MaurizioCr <crispinomaurizio034@gmail.com>
2024-09-16 17:45:39 +0200

    Avvisi

--- src/Component/MyNavbar.jsx
+++ src/Component/MyNavbar.jsx
@@ -54,7 +54,8 @@ const MyNavbar = () => {
                             <Button variant="info" className="mt-1 w-75">Come fare</Button>
                             <Button variant="info" className="mt-1 w-75">News</Button>
                             <Button variant="info" className="mt-1 w-75">Eventi</Button>
-                            <Button variant="info" className="mt-1 w-75">Ufficio in stampa</Button>
+                            <Button variant="info" className="mt-1 w-75">Ufficio stampa</Button>
+                            <Button variant="info" className="mt-1 w-75">Avvisi</Button>
                         </Row>
             case 'contatti':
                 return <p>Contatti content goes here.</p>;
