fd87f50c4b7468c3f52f36584112ed7c3a3dffb9
MaurizioCr <crispinomaurizio034@gmail.com>
2024-10-14 13:55:56 +0200

    Carosello

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,4 +1,4 @@
-import { Col, Container, Row, Nav } from "react-bootstrap";
+import { Col, Container, Row, Nav, Carousel } from "react-bootstrap"; // Importa Carousel
 import { HouseDoorFill, InfoCircleFill, PersonFillCheck } from "react-bootstrap-icons";
 import numeroverde from "../Assets/numero.png";
 
@@ -6,33 +6,38 @@ const Home = () => {
     return (
     <>
         <Container className="rounded text-center text-white bg-primary mt-3 py-3 w-75">
-                <Row>
-                    <Col>
-                        <Nav.Link><span><HouseDoorFill/></span>Asl a casa tua</Nav.Link>
-                    </Col>
-                    <Col>
-                        <Nav.Link><span><PersonFillCheck/></span>Vaccinazioni</Nav.Link>
-                    </Col>
-                    <Col>
-                        <Nav.Link><span><InfoCircleFill/></span>Informazioni</Nav.Link>
-                    </Col>
-                </Row>
+            <Row>
+                <Col>
+                    <Nav.Link><span><HouseDoorFill/></span>Asl a casa tua</Nav.Link>
+                </Col>
+                <Col>
+                    <Nav.Link><span><PersonFillCheck/></span>Vaccinazioni</Nav.Link>
+                </Col>
+                <Col>
+                    <Nav.Link><span><InfoCircleFill/></span>Informazioni</Nav.Link>
+                </Col>
+            </Row>
         </Container>
 
-        <Row className="text-center mt-3"> 
-            <Col className="px-0"> 
-                <img src={numeroverde} className=""  alt="Prevenzione" />
-            </Col>
-        </Row>
-        <Container className="text-center mt-3 mb-5">
-            <h2>Prenota da casa</h2>
-            <p>Grazie al nuovo numero verde potrai prenotare da casa le visite mediche che necessiti.</p>
-        </Container>
+        <Carousel className="mt-3" interval={3000} controls={true} indicators={true}>
+            <Carousel.Item>
+                <Row className="text-center mt-3"> 
+                       <Col className="px-0"> 
+                            <img src={numeroverde} className=""  alt="Numero Verde" />
+                        </Col>
+                </Row>
+                <Row className="text-center mt-3 mb-5">
+                    <h2>Prenota da casa</h2>
+                    <p>Grazie al nuovo numero verde potrai prenotare da casa le visite mediche che necessiti.</p>
+                </Row>
+            </Carousel.Item>
+        </Carousel>
+
+        
 
         <Container className="text-start mt-5">
             <h2>In evidenza</h2>
         </Container>
-
     </>
     )
 };
