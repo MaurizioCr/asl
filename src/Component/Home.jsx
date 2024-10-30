bcf87fad5208e5133158c801298388d617330a18
MaurizioCr <crispinomaurizio034@gmail.com>
2024-10-30 11:20:28 +0100

    Stle col 2

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -51,27 +51,27 @@ const Home = () => {
             <Container className="text-start mt-5">
                 <h2>In evidenza</h2>
                 <Row>
-                    <Col className="col-3 border border-black ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow">
+                    <Col className="col-3 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow">
                         <h2>Esenzioni Ticket </h2>
                         <Button>Approfondisci</Button>
                     </Col>
-                    <Col className="col-3 border border-black ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow">
+                    <Col className="col-3 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow">
                         <h2>Fascicolo Elettronico </h2>
                         <Button>Approfondisci</Button>
                     </Col>
-                    <Col className="col-4 border border-black ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow">
+                    <Col className="col-4 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow">
                         <h2>Scelta/revoca medico o pediatra </h2>
                         <Button>Approfondisci</Button>
                     </Col>
-                    <Col className="col-3 border border-black ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow">
+                    <Col className="col-3 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow">
                         <h2>118 - Emergenza ed Urgenza Pre-Ospedaliera</h2>
                         <Button>Approfondisci</Button>
                     </Col>
-                    <Col className="col-3 border border-black ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow">
+                    <Col className="col-3 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow">
                         <h2>Prenota le tue visite ed esami</h2>
                         <Button>Approfondisci</Button>
                     </Col>
-                    <Col className="col-4 border border-black ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow">
+                    <Col className="col-4 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow">
                         <h2>Rete Oncologica</h2>
                         <Button>Approfondisci</Button>
                     </Col>
