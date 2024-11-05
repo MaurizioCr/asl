382510a40e816552b71cdff1f23d3a9c96ec6a03
MaurizioCr <crispinomaurizio034@gmail.com>
2024-11-05 15:10:19 +0100

    style

--- src/Component/News.jsx
+++ src/Component/News.jsx
@@ -4,15 +4,24 @@ const News = () => {
     return (
         <>
          <Container>
-            <h2>Ultime notizie</h2>
+            <h1 className="mb-5">Ultime notizie</h1>
             <Row>
-                <h3>Atto aziendale</h3>
-                    <Col className="col-2 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow bg-primary ">
+                <Col className="col-3 d-flex flex-column justify-content-between">
+                    <h3>Atto aziendale</h3>
+                </Col>
+
+                <Col className="col-3 ms-3 d-flex flex-column justify-content-between">
+                    <h3>PNES - Contrastare la povertà</h3>
+                </Col>
+            <Row>
+            </Row>
+                
+                    <Col className="col-3 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow bg-primary ">
                         <p>Atto Aziendale dell’ Azienda Sanitaria Locale Napoli 2 nord adottato con la Delibera aziendale 2131 del 21-11-2023 </p>
                         <Button variant="secondary">Leggi</Button>
                     </Col>
-                <h3>PNES - Contrastare la povertà</h3>
-                    <Col className="col-2 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow bg-primary ">
+                
+                    <Col className="col-3 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow bg-primary ">
                         <p>Il Programma Nazionale Equità nella Salute 2021-2027 interviene per rafforzare i servizi sanitari e renderne più equo l’accesso </p>
                         <Button variant="secondary">Leggi</Button>
                     </Col>
