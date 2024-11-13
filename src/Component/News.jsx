8d5c8060f4e74557a1b47e39720d4310b9d63cb5
MaurizioCr <crispinomaurizio034@gmail.com>
2024-11-13 15:22:09 +0100

    Fix seconda row

--- src/Component/News.jsx
+++ src/Component/News.jsx
@@ -36,20 +36,22 @@ const News = () => {
                 <Col className="col-3 ms-3 d-flex flex-column justify-content-between">
                     <h3>Scegli come prenotare</h3>
                 </Col>
-                <Col className="col-3 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow bg-primary ">
-                    <p>Per limitare il flusso di assistiti negli Ospedali e nei Distretti, viene garantita l’assistenza sanitaria in sicurezza presso tutte le strutture</p>
-                    <Button variant="secondary">Leggi</Button>
-                </Col>
                 <Col className="col-3 ms-3 d-flex flex-column justify-content-between">
                     <h3>Progetti Finanziati con fondi a specifica destinazione</h3>
-                </Col>
-                <Col className="col-3 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow bg-primary ">
-                    <p>L’Azienda è particolarmente interessata a sviluppare e implementare progetti di ricerca dal carattere fortemente innovativo.</p>
-                    <Button variant="secondary">Leggi</Button>
                 </Col>
                 <Col className="col-3 ms-3 d-flex flex-column justify-content-between">
                     <h3>Piano regionale prevenzione 2020-2025</h3>
                 </Col>
+                <Col className="col-3 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow bg-primary ">
+                    <p>Per limitare il flusso di assistiti negli Ospedali e nei Distretti, viene garantita l’assistenza sanitaria in sicurezza presso tutte le strutture</p>
+                    <Button variant="secondary">Leggi</Button>
+                </Col>
+                
+                <Col className="col-3 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow bg-primary ">
+                    <p>L’Azienda è particolarmente interessata a sviluppare e implementare progetti di ricerca dal carattere fortemente innovativo.</p>
+                    <Button variant="secondary">Leggi</Button>
+                </Col>
+                
                 <Col className="col-3 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow bg-primary ">
                     <p>È lo strumento di pianificazione centrale degli interventi di prevenzione e promozione della salute, da realizzare sul territorio.</p>
                     <Button variant="secondary">Leggi</Button>
