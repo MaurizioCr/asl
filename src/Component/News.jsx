d00e29c7df69b05f4b3fae841a41b3e2459ea494
MaurizioCr <crispinomaurizio034@gmail.com>
2024-11-19 15:24:08 +0100

    changing col with card 2

--- src/Component/News.jsx
+++ src/Component/News.jsx
@@ -19,33 +19,45 @@ const News = () => {
     return (
         <>
             <Container className="square border border-5 px-3 py-3">
-            <h1 className="mb-5">Ultime notizie</h1>
-            <Row className="mb-5 ">
-                <Col className="col-3 d-flex flex-column justify-content-between">
-                    <h3>Atto aziendale</h3>
-                </Col>
-
-                <Col className="col-3 ms-3 d-flex flex-column justify-content-between">
-                    <h3>PNES - Contrastare la povertà</h3>
-                </Col>
-                <Col className="col-3 ms-3 d-flex flex-column justify-content-between">
-                    <h3>Nomenclatore Tariffario Regionale</h3>
-                </Col>
-                
-                <Col className="col-3 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow bg-primary ">
-                    <p>Atto Aziendale dell’ Azienda Sanitaria Locale Napoli 2 nord adottato con la Delibera aziendale 2131 del 21-11-2023 </p>
-                    <Button variant="secondary">Leggi</Button>
-                </Col>            
-                <Col className="col-3 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow bg-primary ">
-                    <p>Il Programma Nazionale Equità nella Salute 2021-2027 interviene per rafforzare i servizi sanitari e renderne più equo l’accesso </p>
-                    <Button variant="secondary">Leggi</Button>
-                </Col>
-                <Col className="col-3 border border-gray ps-3 py-3 m-2 d-flex flex-column justify-content-between shadow bg-primary ">
-                    <p>Contiene l'elenco delle prestazioni di assistenza specialistica ambulatoriale con le relative tariffe, erogabili dal SSR </p>
-                    <Button variant="secondary">Leggi</Button>
-                </Col>
+                <h1 className="mb-5">Ultime notizie</h1>
+                <Row className="mb-5 ">
                     
-            </Row>
+                        <Col md={4} className="d-flex flex-column">
+                            <h3 style={titleStyle}>Atto aziendale</h3>
+                                <Card className="h-100">
+                                    <Card.Body style={cardBodyStyle} className="bg-primary">
+                                        <Card.Text>
+                                            Atto Aziendale dell’ Azienda Sanitaria Locale Napoli 2 nord adottato con la Delibera aziendale 2131 del 21-11-2023
+                                        </Card.Text>
+                                        <Button variant="secondary">Leggi</Button>
+                                    </Card.Body>
+                                </Card>
+                        </Col>
+
+                        <Col md={4} className="d-flex flex-column">
+                            <h3 style={titleStyle}>PNES - Contrastare la povertà</h3>
+                                <Card className="h-100">
+                                    <Card.Body style={cardBodyStyle} className="bg-primary">
+                                        <Card.Text>
+                                            Il Programma Nazionale Equità nella Salute 2021-2027 interviene per rafforzare i servizi sanitari e renderne più equo l’accesso 
+                                        </Card.Text>
+                                        <Button variant="secondary">Leggi</Button>
+                                    </Card.Body>
+                                </Card>
+                        </Col>
+
+                        <Col md={4} className="d-flex flex-column">
+                            <h3 style={titleStyle}>Nomenclatore Tariffario Regionale</h3>
+                                <Card className="h-100">
+                                    <Card.Body style={cardBodyStyle} className="bg-primary">
+                                        <Card.Text>
+                                            Contiene l'elenco delle prestazioni di assistenza specialistica ambulatoriale con le relative tariffe, erogabili dal SSR 
+                                        </Card.Text>
+                                        <Button variant="secondary">Leggi</Button>
+                                    </Card.Body>
+                                </Card>
+                        </Col>
+                </Row>
 
 
 
