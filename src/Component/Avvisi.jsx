a63f2e2ea0af453f253e8c4f532df85a657fb17b
MaurizioCr <crispinomaurizio034@gmail.com>
2025-01-08 15:11:31 +0100

    as

--- src/Component/Avvisi.jsx
+++ src/Component/Avvisi.jsx
@@ -5,10 +5,6 @@ const Avvisi = () => {
     <>
     <Container className="text-start mt-5 mb-5">
                 <h2>Avvisi</h2>
-                <Row>
-                Manifestazione di interesse all'Avviso Interno UOC Formazione, Aggiornamento e Poli Didattici per la proposta di incarico triennale di Coordinatore delle attività formative, professionalizzanti e di tirocinio nel Corso di Laurea in Infermieristica dell'Università degli Studi della Campania luigi Vanvitelli - Sede Caivano
-                <p>18 Ottobre 2024</p>
-                </Row>
             </Container>
     </>
         );
