af2fb7468f555c21211acf7ddee04aeee0571895
MaurizioCr <crispinomaurizio034@gmail.com>
2025-01-08 15:27:25 +0100

    Secondo avviso

--- src/Component/Avvisi.jsx
+++ src/Component/Avvisi.jsx
@@ -19,18 +19,28 @@ const Avvisi = () => {
     <Container className="text-start mt-5 mb-5">
                 <h2>Avvisi</h2>
                 <Row>
-                <Col md={4} className="d-flex flex-column">
-                                <Card className="h-100">
-                                    <Card.Body style={cardBodyStyle} className="bg-primary">
-                                        <Card.Text>
-                                        Manifestazione di interesse all'Avviso Interno UOC Formazione, Aggiornamento e Poli Didattici per la proposta di incarico triennale di Coordinatore delle attività formative, professionalizzanti e di tirocinio nel Corso di Laurea in Infermieristica dell'Università degli Studi della Campania luigi Vanvitelli - Sede Caivano
-
-                                        </Card.Text>
-                                        <Card.Footer>18 Ottobre 2024</Card.Footer>
-                                        <Button variant="secondary">Leggi</Button>
-                                    </Card.Body>
-                                </Card>
-                        </Col>
+                    <Col md={4} className="d-flex flex-column">
+                        <Card className="h-100">
+                            <Card.Body style={cardBodyStyle} className="bg-primary">
+                               <Card.Text>
+                                    Convocazione prova colloquio - Avviso pubblico mobilità intraregionale, per titoli e colloquio, per la copertura a tempo pieno ed indeterminato a n.1. posto di Dirigente Veterinario - Area A - Sanità animale
+                                </Card.Text>
+                                <Card.Footer>18 Ottobre 2024</Card.Footer>
+                                <Button variant="secondary">Leggi</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="d-flex flex-column">
+                        <Card className="h-100">
+                            <Card.Body style={cardBodyStyle} className="bg-primary">
+                               <Card.Text>
+                               Convocazione prova colloquio - Avviso pubblico mobilità intraregionale, per titoli e colloquio, per la copertura a tempo pieno ed indeterminato a n.1. posto di Dirigente Veterinario - Area A - Sanità animale
+                                </Card.Text>
+                                <Card.Footer>17 Ottobre 2024</Card.Footer>
+                                <Button variant="secondary">Leggi</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
                 </Row>
             </Container>
     </>
