793f195279ca253cea34ebc95e21d6a32a607608
MaurizioCr <crispinomaurizio034@gmail.com>
2025-01-08 15:23:45 +0100

    Primo Avviso

--- src/Component/Avvisi.jsx
+++ src/Component/Avvisi.jsx
@@ -1,13 +1,36 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
 const Avvisi = () => {
+    const titleStyle = {
+        minHeight: "70px", // per non avere dislivelli
+            display: "flex",
+            alignItems: "flex-start",
+        };
+        
+
+    const cardBodyStyle = {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "100%",
+    };
     return (
     <>
     <Container className="text-start mt-5 mb-5">
                 <h2>Avvisi</h2>
                 <Row>
-                Manifestazione di interesse all'Avviso Interno UOC Formazione, Aggiornamento e Poli Didattici per la proposta di incarico triennale di Coordinatore delle attività formative, professionalizzanti e di tirocinio nel Corso di Laurea in Infermieristica dell'Università degli Studi della Campania luigi Vanvitelli - Sede Caivano
-                <p>18 Ottobre 2024</p>
+                <Col md={4} className="d-flex flex-column">
+                                <Card className="h-100">
+                                    <Card.Body style={cardBodyStyle} className="bg-primary">
+                                        <Card.Text>
+                                        Manifestazione di interesse all'Avviso Interno UOC Formazione, Aggiornamento e Poli Didattici per la proposta di incarico triennale di Coordinatore delle attività formative, professionalizzanti e di tirocinio nel Corso di Laurea in Infermieristica dell'Università degli Studi della Campania luigi Vanvitelli - Sede Caivano
+
+                                        </Card.Text>
+                                        <Card.Footer>18 Ottobre 2024</Card.Footer>
+                                        <Button variant="secondary">Leggi</Button>
+                                    </Card.Body>
+                                </Card>
+                        </Col>
                 </Row>
             </Container>
     </>
